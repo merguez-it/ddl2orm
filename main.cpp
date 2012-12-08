609487d5a9375955d3f97b7caf284c2db6523c73
Mathias Franck <mathias.franck@vidal.fr>
2012-12-09 00:36:47 +0100

    Fix? added .pbxuser, to keep launch-test args. Fix: crash of ddl2lorm when argv[1] was null. ..Hope this could be cloned->built->tested now :o(

--- main.cpp
+++ main.cpp
@@ -17,21 +17,25 @@
 using namespace std;
 
 int main (int argc, char * const argv[]) {
-	
 	//TODO: Usage...
 	int err=noErr;
-	string fileName = argv[1];
-	string outputDir="./"; 
-	if (argc >2) outputDir=argv[2]; 
-	ObjectModel *model=new ObjectModel(fileName,outputDir);
-	err=model->parseDDLtoObjectModel();
-	if (noErr==err) {
-		MappedTables bidir_assoces=model->pure_associations_tables();
-		std::wcout << L"Total mapped tables : " << model->mapped_tables().size() << std::endl;
-		std::wcout << L"Mapped pure associations: " << bidir_assoces.size() << std::endl;
-		OrmGenerator gen(*model);
-		err=gen.generateFiles();
+	if (!argv[1]) {
+		std::cout << "An SQLite script file path should be given - quitting..." << endl;
+		err=-1;
+	} else {
+		string fileName = argv[1];
+		string outputDir="./"; 
+		if (argc >2) outputDir=argv[2]; 
+		ObjectModel *model=new ObjectModel(fileName,outputDir);
+		err=model->parseDDLtoObjectModel();
+		if (noErr==err) {
+			MappedTables bidir_assoces=model->pure_associations_tables();
+			std::wcout << L"Total mapped tables : " << model->mapped_tables().size() << std::endl;
+			std::wcout << L"Mapped pure associations: " << bidir_assoces.size() << std::endl;
+			OrmGenerator gen(*model);
+			err=gen.generateFiles();
+		}
+		delete model;
 	}
-	delete model;
 	return err;
 }
